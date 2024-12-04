0075942afb35634c8276f401309706a7934ab988
liwangivone <liwangivone@gmail.com>
2024-12-04 10:44:16 +0800

    final commit

---
+++ route_navigation/lib/first_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'second_screen.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to the second screen.
+            Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+          },
+          child: const Text('Go to Second Screen'),
+        ),
+      ),
+    );
+  }
+}
